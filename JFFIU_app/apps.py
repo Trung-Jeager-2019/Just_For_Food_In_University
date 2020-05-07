21ce95842baf320d6d9c23deeefdb2a57a09c04b
Trung-Jeager-2019 <Trung-Jeager-2019@github.com>
2020-05-07 18:19:50 +0700

    first commit

---
+++ JFFIU_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JffiuAppConfig(AppConfig):
+    name = 'JFFIU_app'
---
+++ pip_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Documents\Study_in_University\Third_year\HKII\Developing_Web_Application\Just_For_Food_In_University\pip_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
