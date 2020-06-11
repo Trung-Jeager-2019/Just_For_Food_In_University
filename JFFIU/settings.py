1d9ed81d275524462785e5ab662ca7f2732e8658
Trung-Jeager-2019 <trungjeager2020@Dell-Precision-5510.localdomain>
2020-06-11 23:03:15 +0700

    fix db

--- JFFIU/settings.py
+++ JFFIU/settings.py
@@ -84,13 +84,23 @@ WSGI_APPLICATION = 'JFFIU.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jffiu',
+        'USER': 'jffiu_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
---
+++ linux_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/trungjeager2020/industry_project/ptud_web/Just_For_Food_In_University/linux_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
